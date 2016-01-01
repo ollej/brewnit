eca93b65dbd22fe84fd94bbb56393bf61f48a87c
Olle Johansson <olle@avidity.se>
2016-01-01 22:19:46 +0100

    Updated all references to domain name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'brewnit.app.ollej.com' }
+  config.action_mailer.default_url_options = { :host => 'brygglogg.se' }
 end
