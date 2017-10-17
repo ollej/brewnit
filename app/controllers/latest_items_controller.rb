c243070e04b499718b4b93d046bb7b95c388e628
Olle Johansson <Olle@Johansson.com>
2017-10-17 20:48:16 +0200

    Skip comments where creator is missing. Refactor into class.

--- app/controllers/latest_items_controller.rb
+++ app/controllers/latest_items_controller.rb
@@ -2,18 +2,6 @@ class LatestItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @latest = latest_items
-  end
-
-  private
-
-  def latest_items
-    (User.latest + Recipe.latest + latest_comments).sort_by(&:created_at).reverse
-  end
-
-  def latest_comments
-    Commontator::Comment.where(deleted_at: nil).limit(10).order('created_at desc').reject do |comment|
-      comment.thread.commontable.nil? || !comment.thread.can_be_read_by?(current_user)
-    end
+    @latest = LatestItems.new(current_user).all
   end
 end
