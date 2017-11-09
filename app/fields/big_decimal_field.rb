886be6b16c34057a490ebfa0dd3045a80e4f9eed
Olle Johansson <Olle@Johansson.com>
2017-11-09 19:45:35 +0100

    Fix possible nil exception.

--- app/fields/big_decimal_field.rb
+++ app/fields/big_decimal_field.rb
@@ -2,6 +2,6 @@ require "administrate/field/base"
 
 class BigDecimalField < Administrate::Field::Base
   def to_s
-    data.round(options.fetch(:decimals, 2))
+    data&.round(options.fetch(:decimals, 2))
   end
 end
