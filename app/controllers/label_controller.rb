745d318ad307f1e4cbec9c2e549051906d9df9ac
Olle Johansson <olle@wreede.se>
2019-03-12 23:22:08 +0100

    Generate label pdf files via Py3o.Fusion
    * Add Py3o:Fusion docker containers to Vagrant.
    * Upgrade Linux kernel to 4.4 in Vagramnt.
    * Support X-Accel-Redirect to send files via Nginx.
    * Upgrade py3o_fusion version

--- app/controllers/label_controller.rb
+++ app/controllers/label_controller.rb
@@ -7,37 +7,53 @@ class LabelController < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:id])
-    pdf = Tempfile.new("etiketter.pdf")
-    generate(pdf, data)
-    respond_to do |format|
-      format.pdf do
-        send_data(pdf, filename: 'etiketter.pdf', type: 'application/pdf')
-      end
-    end
-    pdf.unlink
+    pdf = Tempfile.new "etiketter.pdf", Rails.root.join("pdf")
+    Rails.logger.debug { "path to generated label file: #{pdf.path}" }
+    generate pdf, data
+    send_file pdf.path, filename: 'etiketter.pdf', type: :pdf, disposition: :attachment
+    #pdf.unlink # FIXME: Needs cleanup, as we need to keep this until nginx has delivered the file
   end
 
   private
   def generate(file, data)
-    py3o = Py3oFusion.new(ENV.fetch('PY3OFUSION_ENDPOINT'))
-      .template("files/label_template.odt")
-      .data(data)
-    if @recipe.user.media_brewery.present?
+    Rails.logger.debug { "Endpoint: #{ENV.fetch('PY3OFUSION_ENDPOINT')}" }
+    Rails.logger.debug { "path to generated label template: #{Rails.root.join('files', 'label_template.odt')}" }
+    Rails.logger.debug { "data: #{data}" }
+    # TODO: Fix py3o.fusion so it only needs each image once
+    # TODO: Create background job to create pdf
+    # TODO: Setup X-Accel-Redirect and X-Accel-Mapping in dokku nginx
+    # TODO: Setup X-Accel-Redirect and X-Accel-Mapping in vagrant nginx
+    #if @recipe.user.media_brewery.present?
+    if true
       (1..9).each do |i|
-        py3o.static_image("logo#{i}", @recipe.user.media_brewery.file)
+        # TODO: Seems to require all images
+        #py3o_fusion.static_image("logo#{i}", @recipe.user.media_brewery.file)
+        py3o_fusion.static_image("logo#{i}", @recipe.user.media_avatar.file.path)
       end
     end
     (1..9).each do |i|
-      py3o.static_image("qrcode{i}", qrcode)
+      py3o_fusion.static_image("qrcode#{i}", qrcode.path)
     end
-    py3o.generate_pdf(file)
-    @qrcode.unlink
+    Rails.logger.debug { py3o_fusion.send(:payload) }
+    py3o_fusion.generate_pdf(file.path)
+    file.rewind
+    qrcode.unlink
+  end
+
+  def py3o_fusion
+    @py3o ||= Py3oFusion.new(
+      ENV.fetch('PY3OFUSION_ENDPOINT'),
+      logger: Rails.logger,
+      timeout: 240)
+      .template(Rails.root.join('files', 'label_template.odt'))
+      .data(data)
   end
 
   def qrcode
     return @qrcode if @qrcode.present?
-    @qrcode = Tempfile.new('qrcode.svg')
-    @qrcode.write(RQRCode::QRCode.new(recipe_url(@recipe)).as_svg)
+    @qrcode = Tempfile.new('qrcode.png')
+    RQRCode::QRCode.new(recipe_url(@recipe)).as_png(file: @qrcode.path)
+    @qrcode.close
     @qrcode
   end
 
