d9291db464ca630ea8d1bcdbfa6c85f5e3f39cae
Olle Johansson <olle@wreede.se>
2019-03-17 22:52:15 +0100

    Fix production issues of beer label preview

--- app/helpers/label_helper.rb
+++ app/helpers/label_helper.rb
@@ -1,5 +1,5 @@
 module LabelHelper
   def label_svg(label)
-    inline_svg(Rails.root.join('app', 'assets', 'labeltemplates', label))
+    inline_svg(Rails.root.join('app', 'assets', 'labeltemplates', label).to_s)
   end
 end
