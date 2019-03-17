48fb63e9787225e89d552d82c57f99e52fbc5fda
Olle Johansson <olle@wreede.se>
2019-03-17 15:56:10 +0100

    Fix specs for label controller

--- spec/controllers/label_controller_spec.rb
+++ spec/controllers/label_controller_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe LabelController, type: :controller do
+  let(:recipe) { Recipe.create(recipe_attributes) }
+  let(:recipe_attributes) do
+    {
+      user: user,
+      name: 'recipe test'
+    }
+  end
+
+  login_user
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, params: { id: recipe.id }
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
+      get :create, params: { id: recipe.id }
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq "application/pdf"
     end
   end
-
 end
