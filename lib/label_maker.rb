bb4dc02e9d22f0f277a63be3221e9017bbedea12
Olle Johansson <olle@wreede.se>
2019-03-17 19:05:46 +0100

    Display attribution line on label pdf

--- lib/label_maker.rb
+++ lib/label_maker.rb
@@ -1,3 +1,5 @@
+require  "prawn/measurement_extensions"
+
 class LabelMaker
   def initialize(template)
     @template = template
@@ -9,6 +11,13 @@ class LabelMaker
 
   private
   def build
+    render_labels
+    render_footer(90.mm)
+    render_footer(188.mm)
+    pdf
+  end
+
+  def render_labels
     %i(top center bottom).each do |vposition|
       %i(left center right).each do |hposition|
         pdf.svg @template, {
@@ -18,13 +27,27 @@ class LabelMaker
         }
       end
     end
-    pdf
+    self
+  end
+
+  def render_footer(hposition)
+    path = Rails.root.join('app', 'assets', 'images', 'brygglogg-logo-hires.png')
+    pdf.image path, width: 1.cm, at: [25.mm, hposition]
+    pdf.image path, width: 1.cm, at: [145.mm, hposition]
+    pdf.font "Merriweather" do
+      pdf.text_box "Baksidesetiketter utskrivna från Brygglogg.se",
+        at: [42.mm, hposition - 2.mm]
+    end
+    self
   end
 
   def pdf
+    return @pdf if @pdf.present?
     @pdf ||= Prawn::Document.new(
       page_size: "A4",
       margin: 42
     )
+    @pdf.font_families.update("Merriweather" => { normal: Rails.root.join('app', 'assets', 'fonts', 'merriweather.ttf') })
+    @pdf
   end
 end
