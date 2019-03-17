718256750ca5f4c734ce79d7013fbb64891972f1
Olle Johansson <olle@wreede.se>
2019-03-17 17:08:12 +0100

    Cleanup display of data on beer label

--- lib/label_template.rb
+++ lib/label_template.rb
@@ -28,11 +28,18 @@ class LabelTemplate
     content("#beerdescription2", description2)
     content("#beerdescription3", description3)
     content("#beerdescription4", description4)
-    maybe_value("#beerdetails1", "ABV", abv)
-    maybe_value("#beerdetails2", "IBU", ibu)
-    maybe_value("#beerdetails3", "EBC", ebc)
-    content("#beerdetails6", bottledate)
-    content("#bottlesize > tspan", bottlesize) if bottlesize.present?
+    details([
+      Detail.new("ABV", abv),
+      Detail.new("IBU", ibu),
+      Detail.new("EBC", ebc)
+    ])
+    if bottledate.present?
+      content("#beerdetails6", bottledate)
+    else
+      clear("#beerdetails5")
+      clear("#beerdetails6")
+    end
+    content("#bottlesize > tspan", bottlesize)
     image("#logo", logo) if logo.present?
     image("#qrcode", qrcode) if qrcode.present?
     @doc
@@ -42,13 +49,20 @@ class LabelTemplate
     @doc.at_css(css).content = content
   end
 
-  def maybe_value(css, header, value)
-    content = if value.present?
-                "#{header}: #{value}"
-              else
-                ""
-              end
-    @doc.at_css(css).content = content
+  def clear(css)
+    content(css, "")
+  end
+
+  def details(details)
+    details.select! { |detail| detail.has_value? }
+    3.times do |index|
+      detail = details.fetch(index, Detail.new)
+      detail("#beerdetails#{index + 1}", detail)
+    end
+  end
+
+  def detail(css, detail)
+    content(css, detail.content)
   end
 
   def image(css, file)
@@ -67,5 +81,26 @@ class LabelTemplate
 
   def sanitize_field(value)
     html_entities.decode Rails::Html::WhiteListSanitizer.new.sanitize(value, tags: [], attributes: [])
+  end
+
+  class Detail
+    attr_reader :name, :value
+
+    def initialize(name = nil, value = nil)
+      @name = name
+      @value = value
+    end
+
+    def has_value?
+      @value.present?
+    end
+
+    def content
+      if has_value?
+        "#{@name}: #{@value}"
+      else
+        ""
+      end
+    end
   end
 end
