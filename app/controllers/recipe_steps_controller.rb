0eaefa053b53b87e6fdcf8b9c6caafdbbad8366a
Olle Wreede <olle@wreede.se>
2020-06-22 20:43:32 +0200

    Send pushover message when user starts brewtimer

--- app/controllers/recipe_steps_controller.rb
+++ app/controllers/recipe_steps_controller.rb
@@ -4,6 +4,7 @@ class RecipeStepsController < ApplicationController
   def show
     @recipe = find_recipe
     raise AuthorizationException unless can_show?(@recipe)
+    PushMessage.new(notification).notify
     @brew_steps = BrewStepsPresenter.new(@recipe)
   end
 
@@ -11,5 +12,18 @@ class RecipeStepsController < ApplicationController
 
   def find_recipe
     Recipe.unscoped.find(params[:id])
+  end
+
+  def notification
+    {
+      title: I18n.t('common.notification.brewtimer.opened.title'),
+      message: I18n.t(
+        'common.notification.brewtimer.opened.message', {
+          user: current_user.display_name,
+          recipe: @recipe.name
+      }),
+      sound: "gamelan",
+      url: recipe_url(@recipe)
+    }
   end
 end
