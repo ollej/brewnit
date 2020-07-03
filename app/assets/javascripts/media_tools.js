33fa54691411218c3faff9faa47a75969f308fd4
Olle Wreede <olle@wreede.se>
2020-07-03 15:58:12 +0200

    Always show image buttons so they work on mobile

--- app/assets/javascripts/media_tools.js
+++ app/assets/javascripts/media_tools.js
@@ -1,8 +1,6 @@
 class MediaTools {
   constructor(thumbnail, btnsel, slider) {
     this.removeMedium = this.removeMedium.bind(this);
-    this.hoverOver = this.hoverOver.bind(this);
-    this.hoverOut = this.hoverOut.bind(this);
     this.thumbnail = thumbnail;
     this.btnsel = btnsel;
     this.slider = slider;
@@ -10,8 +8,6 @@ class MediaTools {
   }
 
   init() {
-    this.$slider.on("mouseenter", this.thumbnail, this.hoverOver);
-    this.$slider.on("mouseleave", this.thumbnail, this.hoverOut);
     this.$slider.on("ajax:success", ".destroy-button", this.removeMedium);
     $(".fancybox").fancybox({
       openEffect: "elastic",
@@ -25,22 +21,6 @@ class MediaTools {
     //console.log(ev.target, $(ev.target).parents(@thumbnail))
     $(ev.target).parents(this.thumbnail).remove();
     this.$slider.trigger("media:update");
-  }
-
-  hoverOver(ev) {
-    const $medium = $(ev.currentTarget);
-    const $btn = this.button($medium);
-    //console.log('hoverOver', $medium, $btn)
-    $btn.show().position({
-      my: "right top",
-      at: "right-4 top+4",
-      of: $medium.find("img")
-    });
-  }
-
-  hoverOut(ev) {
-    const $medium = $(ev.currentTarget);
-    this.button($medium).hide();
   }
 
   button($medium) {
