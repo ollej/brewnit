f68496aaeac5ab1a3984be4656d5cf9d0cb57b9c
Olle Wreede <olle@wreede.se>
2021-03-07 17:53:19 +0100

    Upgrade to Rails 6.1.3

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '6.0.3.5'
+gem 'rails', '6.1.3'
 
 gem 'bootsnap'
 # Use sqlite3 as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
   scope :by_query, lambda { |query, col = :name|
     where arel_table[col].matches("%#{query}%")
   }
-  scope :ordered, -> { order("users.name = '', users.name ASC, brewery ASC") }
+  scope :ordered, -> { order("users.name ASC, brewery ASC") }
   scope :confirmed, -> { where('confirmed_at IS NOT NULL') }
   scope :unconfirmed, -> { where( 'confirmed_at is NULL AND confirmation_sent_at < ?', Time.now - Devise.confirm_within) }
   scope :latest, -> { confirmed.limit(10).order('created_at desc') }
