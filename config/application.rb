896ad04becaed6f64327675589c5a81d546e0f5b
Olle Wreede <olle@wreede.se>
2023-05-04 15:19:15 +0200

    Update cache_format_version

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Brewnit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
